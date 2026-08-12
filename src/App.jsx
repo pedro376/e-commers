9601d1afed2ca8b5ad0297ccb8e1e16af3793558
ra8a0106-ai <ra8a0106@gmail.com>
2026-08-12 02:37:14 -0700

    Merge manual

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+
+
 import './App.css'
 import './css/global.css'
+import './css/inicio.css'
 
 import NavBar from './components/Navbar'
 import Catalogo from './Componentes/Catalogo/Catalogo'
 import Footer from './components/Footer'
+import Inicio from './pages/Inicio'
 
 function App() {
 
