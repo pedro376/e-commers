b68edd7f690e7f08efeaedea5023f0fe599ea97a
pedro376 <arce.pedro376@gmail.com>
2026-08-12 19:47:54 -0700

    agregar carrito y unir toda la pagina

--- src/data/productos.js
+++ src/data/productos.js
@@ -1,142 +1,142 @@
 export const productos = 
 [
-    {
+{
         id:1,
         nombre:"Julian Quiñones",
         precio:450,
-        img:"public/img_pruebas/playera.jpeg"
-    },
+        img:"/img_pruebas/playera.jpeg"
+},
 
-    {
+{
         id:2,
         nombre:"Erling Haaland",
         precio:540,
-        img:"public/img_pruebas/haaland_azul.jpeg"
-    },
+        img:"/img_pruebas/haaland_azul.jpeg"
+},
 
-    {
+{
         id:3,
         nombre:"Gilberto Mora",
         precio:420,
-        img:"public/img_pruebas/gil_mora.jpeg"
-    },
+        img:"/img_pruebas/gil_mora.jpeg"
+},
 
-    {
+{
         id:4,
         nombre:"CR7",
         precio:700,
-        img:"public/img_pruebas/CR7_portugal.jpeg"
-    },
+        img:"/img_pruebas/CR7_portugal.jpeg"
+},
 
-    {
+{
         id:5,
         nombre:"Kylian Mbappé",
         precio:620,
-        img:"public/img_pruebas/mbappe_blanco.jpeg"
-    },
+        img:"/img_pruebas/mbappe_blanco.jpeg"
+},
 
-    {
+{
         id:6,
         nombre:"Erling Haaland",
         precio:730,
-        img:"public/img_pruebas/haaland_negro.jpeg"
-    },
+        img:"/img_pruebas/haaland_negro.jpeg"
+},
 
-    {
+{
         id:7,
         nombre:"Erling Haaland",
         precio:700,
-        img:"public/img_pruebas/haaland_blanco.jpeg"
-    },
+        img:"/img_pruebas/haaland_blanco.jpeg"
+},
 
-    {
+{
         id:8,
         nombre:"Club Real Madrid",
         precio:680,
-        img:"public/img_pruebas/real_madrid.jpeg"
-    },
+        img:"/img_pruebas/real_madrid.jpeg"
+},
 
-    {
+{
         id:9,
         nombre:"Club Barca",
         precio:630,
-        img:"public/img_pruebas/barca.jpeg"
-    },
+        img:"/img_pruebas/barca.jpeg"
+},
 
-    {
+{
         id:10,
         nombre:"Selección De México",
         precio:450,
-        img:"public/img_pruebas/seleccion_mx_blanco.jpeg"
-    },
+        img:"/img_pruebas/seleccion_mx_blanco.jpeg"
+},
 
-    {
+{
         id:11,
         nombre:"Selección De México",
         precio:400,
-        img:"public/img_pruebas/seleccion_mx_negro.jpeg"
-    },
+        img:"/img_pruebas/seleccion_mx_negro.jpeg"
+},
 
-    {
+{
         id:12,
         nombre:"Selección De Brasil",
         precio:720,
-        img:"public/img_pruebas/seleccion_brasil.jpeg"
-    },
+        img:"/img_pruebas/seleccion_brasil.jpeg"
+},
 
-    {
+{
         id:13,
         nombre:"Selección De México",
         precio:550,
-        img:"public/img_pruebas/tigres.jpeg"
-    },
+        img:"/img_pruebas/tigres.jpeg"
+},
 
-    {
+{
         id:14,
         nombre:"Selección De México",
         precio:450,
-        img:"public/img_pruebas/america.jpeg"
-    },
+        img:"/img_pruebas/america.jpeg"
+},
 
-    {
+{
         id:15,
         nombre:"Julian Quiñones",
         precio:450,
-        img:"public/img_pruebas/playera.jpeg"
-    },
+        img:"/img_pruebas/playera.jpeg"
+},
 
-    {
+{
         id:16,
         nombre:"Erling Haaland",
         precio:540,
-        img:"public/img_pruebas/haaland_azul.jpeg"
-    },
+        img:"/img_pruebas/haaland_azul.jpeg"
+},
 
-    {
+{
         id:17,
         nombre:"Gilberto Mora",
         precio:420,
-        img:"public/img_pruebas/gil_mora.jpeg"
-    },
+        img:"/img_pruebas/gil_mora.jpeg"
+},
 
-    {
+{
         id:18,
         nombre:"CR7",
         precio:700,
-        img:"public/img_pruebas/CR7_portugal.jpeg"
-    },
+        img:"/img_pruebas/CR7_portugal.jpeg"
+},
 
-    {
+{
         id:19,
         nombre:"Kylian Mbappé",
         precio:620,
-        img:"public/img_pruebas/mbappe_blanco.jpeg"
-    },
+        img:"/img_pruebas/mbappe_blanco.jpeg"
+},
 
-    {
+{
         id:20,
         nombre:"Erling Haaland",
         precio:730,
-        img:"public/img_pruebas/haaland_negro.jpeg"
-    },
+        img:"/img_pruebas/haaland_negro.jpeg"
+},
 ]
